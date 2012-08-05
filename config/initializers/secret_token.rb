bbf1da6c8d5d10804ed055fd5847003ed7940c02
ntaoo <ntaoo.g@gmail.com>
2012-08-05 16:03:39 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlainBlog::Application.config.secret_token = 'bce01bcfd4632d59b69bba6d7744048601569713eb4afb3af79576ed346620d77df982fcaae0e2d4f329d7215bc700bdb4b00c49c5b89ca71b43af34eb12cd5f'
